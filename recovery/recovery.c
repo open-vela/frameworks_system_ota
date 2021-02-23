0743ea8e4431cc53812f0d8de8de52f10db081de
liuhaitao <liuhaitao@xiaomi.com>
2021-02-23 20:20:01 +0800

    recovery: restruct lzma decompress function into decode.c
    N/A
    
    Signed-off-by: liuhaitao <liuhaitao@xiaomi.com>

--- recovery/recovery.c
+++ recovery/recovery.c
@@ -37,87 +37,6 @@
  * recovery_main
  ****************************************************************************/
 
-#ifdef CONFIG_LIB_LZMA
-static SRes decode2(CLzmaDec *state, ISeqOutStream *outStream, ISeqInStream *inStream,
-    UInt64 unpackSize)
-{
-  int thereIsSize = (unpackSize != (UInt64)(Int64)-1);
-  Byte inBuf[IN_BUF_SIZE];
-  Byte outBuf[OUT_BUF_SIZE];
-  size_t inPos = 0, inSize = 0, outPos = 0;
-  LzmaDec_Init(state);
-  for (;;)
-  {
-    if (inPos == inSize)
-    {
-      inSize = IN_BUF_SIZE;
-      RINOK(inStream->Read(inStream, inBuf, &inSize));
-      inPos = 0;
-    }
-    {
-      SRes ret;
-      SizeT inProcessed = inSize - inPos;
-      SizeT outProcessed = OUT_BUF_SIZE - outPos;
-      ELzmaFinishMode finishMode = LZMA_FINISH_ANY;
-      ELzmaStatus status;
-      if (thereIsSize && outProcessed > unpackSize)
-      {
-        outProcessed = (SizeT)unpackSize;
-        finishMode = LZMA_FINISH_END;
-      }
-
-      ret = LzmaDec_DecodeToBuf(state, outBuf + outPos, &outProcessed,
-        inBuf + inPos, &inProcessed, finishMode, &status);
-      inPos += inProcessed;
-      outPos += outProcessed;
-      unpackSize -= outProcessed;
-
-      if (outStream)
-        if (outStream->Write(outStream, outBuf, outPos) != outPos)
-          return SZ_ERROR_WRITE;
-
-      outPos = 0;
-
-      if (ret != SZ_OK || (thereIsSize && unpackSize == 0))
-        return ret;
-
-      if (inProcessed == 0 && outProcessed == 0)
-      {
-        if (thereIsSize || status != LZMA_STATUS_FINISHED_WITH_MARK)
-          return SZ_ERROR_DATA;
-        return ret;
-      }
-    }
-  }
-}
-
-static SRes decode(ISeqOutStream *outStream, ISeqInStream *inStream)
-{
-  UInt64 unpackSize;
-  int i;
-  SRes ret = 0;
-
-  CLzmaDec state;
-
-  /* header: 5 bytes of LZMA properties and 8 bytes of uncompretsed size */
-  unsigned char header[LZMA_PROPS_SIZE + 8];
-
-  /* Read and parete header */
-
-  RINOK(SeqInStream_Read(inStream, header, sizeof(header)));
-
-  unpackSize = 0;
-  for (i = 0; i < 8; i++)
-    unpackSize += (UInt64)header[LZMA_PROPS_SIZE + i] << (i * 8);
-
-  LzmaDec_Construct(&state);
-  RINOK(LzmaDec_Allocate(&state, header, LZMA_PROPS_SIZE, &g_Alloc));
-  ret = decode2(&state, outStream, inStream, unpackSize);
-  LzmaDec_Free(&state, &g_Alloc);
-  return ret;
-}
-#endif
-
 int main(int argc, char *argv[])
 {
   int write_fd;
@@ -127,6 +46,7 @@ int main(int argc, char *argv[])
   syslog(LOG_INFO, "Recovery Service start!\n");
 
 #ifdef CONFIG_LIB_LZMA
+
   CFileSeqInStream inStream;
   CFileOutStream outStream;
 
