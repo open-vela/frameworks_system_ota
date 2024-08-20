306a74551b79e418fa8a857b2a2d9da37fa6d2be
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-08-20 18:56:06 +0800

    avb: Add support for upgrade mode
    Usage: `avb_verify -c /ota/vela_tee.bin /etc/key.avb`
    Return: Zero if should continue, otherwise break
    
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- verify/avb_main.c
+++ verify/avb_main.c
@@ -14,20 +14,49 @@
  * limitations under the License.
  */
 
-#include <libavb.h>
-
 #include "avb_verify.h"
+#include <unistd.h>
+
+void usage(const char* progname)
+{
+    avb_printf("Usage: %s [-b] [-c] <partition> <key> [suffix]\n", progname);
+    avb_printf("Examples:\n");
+    avb_printf("  1. Boot Verify\n");
+    avb_printf("     %s <partition> <key> [suffix]\n", progname);
+    avb_printf("  2. Upgrade Verify\n");
+    avb_printf("     %s -c <image> <key> [suffix]\n", progname);
+}
 
 int main(int argc, char* argv[])
 {
+    AvbSlotVerifyFlags flags = 0;
     int ret;
 
-    if (argc < 3) {
-        avb_printf("%s <partition> <key> [suffix]\n", argv[0]);
+    while ((ret = getopt(argc, argv, "bch")) != -1) {
+        switch (ret) {
+        case 'b':
+            flags = 0;
+            break;
+        case 'c':
+            flags = AVB_SLOT_VERIFY_FLAGS_NOT_ALLOW_SAME_ROLLBACK_INDEX | AVB_SLOT_VERIFY_FLAGS_NOT_UPDATE_ROLLBACK_INDEX;
+            break;
+        case 'h':
+            usage(argv[0]);
+            return 0;
+            break;
+        default:
+            usage(argv[0]);
+            return 10;
+            break;
+        }
+    }
+
+    if (argc - optind < 2) {
+        usage(argv[0]);
         return 100;
     }
 
-    ret = avb_verify(argv[1], argv[2], argv[3]);
+    ret = avb_verify(argv[optind], argv[optind + 1], argv[optind + 2], flags);
     if (ret != 0)
         avb_printf("%s error %d\n", argv[0], ret);
 
