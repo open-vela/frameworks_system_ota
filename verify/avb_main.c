f45b5e11fdf3a84fe3e06f199636b0f772267adf
jingfei <jingfei@xiaomi.com>
2025-11-18 00:24:41 +0800

    avb_verify:support multi-imgae verify at same time
    1. the key and suffix must be specified by -k <key_path> and -s <suffix>
    if not specified, default key is /etc/key.avb and default suffix is NULL
    
    2. suport verify multi-image
    avb_verify [-V <vbmeta_partition>] [-k <key_path>] [-s <suffix>] <partition1> [<partition2> ...]
    
    Signed-off-by: jingfei <jingfei@xiaomi.com>

--- verify/avb_main.c
+++ verify/avb_main.c
@@ -19,79 +19,103 @@
 
 void usage(const char* progname)
 {
-    avb_printf("Usage: %s [-b] [-i] [-V vbmeta_partition] <partition> <key> [suffix]\n", progname);
-    avb_printf("       %s [-U] <image> <partition> <key>\n", progname);
-    avb_printf("       %s [-I] <partition>\n", progname);
+    avb_printf("Usage:\n");
+    avb_printf("  %s [-i] [-V <vbmeta_partition>] [-k <key_path>] [-s <suffix>] <partition1> [<partition2> ...]\n", progname);
+    avb_printf("  %s [-U <image>] [-k <key_path>] [-s <suffix>] <partition1> [<partition2> ...]\n", progname);
+    avb_printf("  %s [-I] [-V <vbmeta_partition>] <partition1> [<partition2> ...]\n", progname);
+    avb_printf("\n");
 
-    avb_printf("\nExamples\n");
-    avb_printf("  -  Boot Verify\n");
-    avb_printf("     %s <partition> <key> [suffix]\n", progname);
-    avb_printf("  -  Boot Verify use vbmeta partition mode\n");
-    avb_printf("     %s -V <vbmeta_partition> <partition> <key> [suffix]\n", progname);
-    avb_printf("  -  Upgrade Verify\n");
-    avb_printf("     %s -U <image> <partition> <key> [suffix]\n", progname);
-    avb_printf("  -  Image Info\n");
-    avb_printf("     %s -I <image>\n", progname);
-    avb_printf("  -  Image Info use vbmeta partition mode\n");
-    avb_printf("     %s -V <vbmeta_partition> -I <image>\n", progname);
+    avb_printf("Options:\n");
+    avb_printf("  -i                Allow rollback index error\n");
+    avb_printf("  -I                Image info mode (display hash descriptor)\n");
+    avb_printf("  -k <key_path>     Path to the verification key. Default: '/etc/key.avb'\n");
+    avb_printf("  -s <suffix>       Slot suffix, must be '_a' or '_b'. Default: NULL\n");
+    avb_printf("  -U <image>        Upgrade verify mode (verify against given image)\n");
+    avb_printf("  -V <vbmeta_part>  Use vbmeta partition mode, we must specify a vbmeta partition path\n");
+    avb_printf("  -h                Show this help message\n");
+    avb_printf("\n");
+
+    avb_printf("Examples:\n");
+    avb_printf("  1. Boot Verify\n");
+    avb_printf("     %s [-k <key_path>] [-s <suffix>] <partition1> [<partition2> ...]\n", progname);
+    avb_printf("\n");
+    avb_printf("  2. Boot Verify using vbmeta partition mode\n");
+    avb_printf("     %s -V <vbmeta_partition> [-k <key_path>] [-s <suffix>] <partition1> ...\n", progname);
+    avb_printf("\n");
+    avb_printf("  3. Upgrade Verify\n");
+    avb_printf("     %s -U <image> [-k <key_path>] [-s <suffix>] <partition1> [<partition2> ...]\n", progname);
+    avb_printf("\n");
+    avb_printf("  4. Image Info\n");
+    avb_printf("     %s -I <partition1> [<partition2> ...]\n", progname);
+    avb_printf("\n");
+    avb_printf("  5. Image Info using vbmeta partition mode\n");
+    avb_printf("     %s -V <vbmeta_partition> -I <partition1> [<partition2> ...]\n", progname);
 }
 
 int main(int argc, char* argv[])
 {
     struct avb_params_t params = { 0 };
-    bool vbmeta_partition_mode = false;
+    struct avb_hash_desc_t hash_desc;
+    int partition_count;
+    bool info = false;
     int ret;
 
-    while ((ret = getopt(argc, argv, "bhiI:U:V:")) != -1) {
+    params.flags = AVB_SLOT_VERIFY_FLAGS_NO_VBMETA_PARTITION;
+    params.key = "/etc/key.avb";
+
+    while ((ret = getopt(argc, argv, "hiIk:s:U:V:")) != -1) {
         switch (ret) {
-        case 'b':
-            break;
         case 'i':
             params.flags |= AVB_SLOT_VERIFY_FLAGS_ALLOW_ROLLBACK_INDEX_ERROR;
             break;
-        case 'I': {
-            struct avb_hash_desc_t hash_desc;
-            if (vbmeta_partition_mode) {
-                ret = avb_vbmeta_hash_desc(params.vbmeta, optarg, &hash_desc);
-            } else {
-                ret = avb_hash_desc(optarg, &hash_desc);
-            }
-
-            if (ret == AVB_SLOT_VERIFY_RESULT_OK) {
-                avb_hash_desc_dump(&hash_desc);
-                return 0;
-            }
-            return 1;
-        }
+        case 'I':
+            info = true;
+            break;
+        case 'k':
+            params.key = optarg;
+            break;
+        case 's':
+            params.suffix = optarg;
+            break;
         case 'U':
             params.image = optarg;
             break;
         case 'V':
-            vbmeta_partition_mode = true;
+            params.flags &= ~AVB_SLOT_VERIFY_FLAGS_NO_VBMETA_PARTITION;
             params.vbmeta = optarg;
             break;
         case 'h':
-            usage(argv[0]);
-            return 0;
-            break;
         default:
             usage(argv[0]);
-            return 10;
+            return 1;
             break;
         }
     }
 
-    if (argc - optind < 2) {
+    params.partition = (const char* const*)&argv[optind];
+    partition_count = argc - optind;
+
+    if (partition_count < 1) {
         usage(argv[0]);
-        return 100;
+        return 10;
     }
 
-    params.partition = argv[optind];
-    params.key = argv[optind + 1];
-    params.suffix = argv[optind + 2];
+    if (info) {
+        for (int i = 0; i < partition_count; i++) {
+            if (params.flags & AVB_SLOT_VERIFY_FLAGS_NO_VBMETA_PARTITION) {
+                ret = avb_hash_desc(params.partition[i], &hash_desc);
+            } else {
+                ret = avb_vbmeta_hash_desc(params.vbmeta, params.partition[i], &hash_desc);
+            }
 
-    if (!vbmeta_partition_mode) {
-        params.flags |= AVB_SLOT_VERIFY_FLAGS_NO_VBMETA_PARTITION;
+            if (ret != 0) {
+                avb_printf("%s get info error for %s %d\n", argv[0], params.partition[i], ret);
+                return ret;
+            }
+
+            avb_hash_desc_dump(&hash_desc);
+        }
+        return 0;
     }
 
     ret = avb_verify(&params);
